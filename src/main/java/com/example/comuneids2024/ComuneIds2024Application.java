c5ebed96ba017eeca17b72d97c9ed2608b51b4f4
enric <enrico.privitera@gmail.com>
2024-06-07 12:25:37 +0200

    first commit

---
+++ src/main/java/com/example/comuneids2024/ComuneIds2024Application.java
@@ -0,0 +1,13 @@
+package com.example.comuneids2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComuneIds2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComuneIds2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/comuneids2024/ComuneIds2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comuneids2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComuneIds2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
