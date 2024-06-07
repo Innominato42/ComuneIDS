ecb50291027a511db6928dc1bda9d7a503caf91c
enric <enrico.privitera@gmail.com>
2024-06-07 12:31:10 +0200

    prova commit

--- src/main/java/com/example/comuneids2024/ComuneIds2024Application.java
+++ src/main/java/com/example/comuneids2024/ComuneIds2024Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ComuneIds2024Application {
-
+ //TODO CLASSE
     public static void main(String[] args) {
         SpringApplication.run(ComuneIds2024Application.class, args);
     }
