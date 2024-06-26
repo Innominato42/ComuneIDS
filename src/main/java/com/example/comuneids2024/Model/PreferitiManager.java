fdf9612a6cdd5d3501aee558d9348e131dda8e63
RicMar73 <riccardo.marini@studenti.unicam.it>
2024-06-26 18:43:01 +0200

    Preferiti

--- src/main/java/com/example/comuneids2024/Model/PreferitiManager.java
+++ src/main/java/com/example/comuneids2024/Model/PreferitiManager.java
@@ -2,7 +2,6 @@ package com.example.comuneids2024.Model;
 
 import com.example.comuneids2024.Repository.ItineraryRepository;
 import com.example.comuneids2024.Repository.POIRepositoriy;
-import com.example.comuneids2024.Repository.PreferitiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,21 +9,78 @@ import org.springframework.stereotype.Component;
 public class PreferitiManager {
 
     @Autowired
-    private POIRepositoriy poiRepositoriy;
+    private POIRepositoriy poiRepository;
 
     @Autowired
     private ItineraryRepository itineraryRepository;
 
+    /*
     @Autowired
     private PreferitiRepository preferitiRepository;
+    */
 
     @Autowired
     private UtenteAutenticatoManager utenteAutenticatoManager;
 
 
-    public void addPOItoFavourites(Long id, Long idComune, Long idPOI)
-    {
+    public void addPOItoFavourites(Long id, Long idComune, Long idPOI) {
+        UtenteAutenticato utente = utenteAutenticatoManager.getUtente(id);
+        if (utente != null) {
+            POI poi = poiRepository.findById(idPOI).orElse(null);
+            if (poi != null) {
+                utente.addPOIToFavourites(poi);
+                utenteAutenticatoManager.addUtente(utente);
+            } else {
+                throw new RuntimeException("POI non trovato");
+            }
+        } else {
+            throw new RuntimeException("Utente non trovato");
+        }
+    }
 
+    public void removePOIfromFavourites(Long id, Long idPOI) {
+        UtenteAutenticato utente = utenteAutenticatoManager.getUtente(id);
+        if (utente != null) {
+            POI poi = poiRepository.findById(idPOI).orElse(null);
+            if (poi != null) {
+                utente.removePOIFromFavourites(poi);
+                utenteAutenticatoManager.addUtente(utente);
+            } else {
+                throw new RuntimeException("POI non trovato");
+            }
+        } else {
+            throw new RuntimeException("Utente non trovato");
+        }
+    }
+
+    public void addItineraryToFavourites(Long id, Long idComune, Long idItinerary) {
+        UtenteAutenticato utente = utenteAutenticatoManager.getUtente(id);
+        if (utente != null) {
+            Itinerary itinerary = itineraryRepository.findById(idItinerary).orElse(null);
+            if (itinerary != null) {
+                utente.addItineraryToFavourites(itinerary);
+                utenteAutenticatoManager.addUtente(utente);
+            } else {
+                throw new RuntimeException("Itinerario non trovato");
+            }
+        } else {
+            throw new RuntimeException("Utente non trovato");
+        }
+    }
+
+    public void removeItineraryFromFavourites(Long id, Long idItinerary) {
+        UtenteAutenticato utente = utenteAutenticatoManager.getUtente(id);
+        if (utente != null) {
+            Itinerary itinerary = itineraryRepository.findById(idItinerary).orElse(null);
+            if (itinerary != null) {
+                utente.removeItineraryFromFavourites(itinerary);
+                utenteAutenticatoManager.addUtente(utente);
+            } else {
+                throw new RuntimeException("Itinerario non trovato");
+            }
+        } else {
+            throw new RuntimeException("Utente non trovato");
+        }
     }
 
 
--- src/main/java/com/example/comuneids2024/Repository/PreferitiRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.comuneids2024.Repository;
-
-import com.example.comuneids2024.Model.UtenteAutenticato;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PreferitiRepository extends MongoRepository<UtenteAutenticato, Long> {
-}
